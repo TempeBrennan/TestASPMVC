28069c3e80a0e8711f0e46d7b2869a3fd390070b
TempeBrennan <425751342@qq.com>
2019-08-25 20:45:03 +0800

    add datavalidation

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -7,15 +7,30 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        // Validation 影响前端校验和后端的 ModelState.IsValid 属性
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "My Release Date->")]
+        [DataType(DataType.DateTime)]
         public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
 
+        [Range(1, 100)]
+        // DataType 相当于前端的Format
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
